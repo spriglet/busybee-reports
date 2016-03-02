c51c86e0aafd3958f1089f295095d6b085bb6b0b
Micah Bell <redfroglet@gmail.com>
2016-03-01 23:08:35 -0600

    Commit message

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-
-
-
-
-/*
-var cool = require('cool-ascii-faces');
-var 
 var express = require('express');
 var app = express();
 
@@ -18,19 +11,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-    result += cool();
-  response.send(result);
+  response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-*/
