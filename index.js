b70de19a2e345e3e103ea730e0f0366b9f6a5daa
Micah Bell <redfroglet@gmail.com>
2016-03-01 23:21:41 -0600

    Commit message

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
+var superagent = require('superagent');
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,7 +13,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send("TEST Web");
+  
+  superagent.get("http://50.240.52.173:5544/")
+  .end(function(e,res){
+
+  		response.send(res);
+
+  });
 });
 
 
